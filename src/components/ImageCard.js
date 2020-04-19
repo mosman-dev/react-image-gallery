a538435c31e36ace0777a00ad36761ea0821e1eb
Mahmoud Osman <mahmoudosman@Mahmouds-MacBook.local>
2020-04-18 19:35:36 -0700

    first commit

---
+++ src/components/ImageCard.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const ImageCard = ({ image }) => {
+    const tags = image.tags.split(',');
+
+    return (
+    <div className='max-w-sm rounded overflow-hidden shadow-lg'>
+      <img className='w-full' src={image.webformatURL} alt='' />
+      <div className='px-6 py-4'>
+        <div className='font-bold text-purple-500 text-xl mb-2'>Photo by {image.user}</div>
+        <ul>
+          <li>
+            <strong>Views: </strong> {parseFloat(image.views).toLocaleString('en')}
+          </li>
+          <li>
+            <strong>Downloads: </strong> {parseFloat(image.downloads).toLocaleString('en')}
+          </li>
+          <li>
+            <strong>Likes: </strong> {parseFloat(image.likes).toLocaleString('en')}
+          </li>
+        </ul>
+      </div>
+      <div className='px-6 py-4'>
+        {tags.map((tag, index) => (
+            <span key={index} className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2'>
+                #{tag.trim()}
+            </span>
+        ))}
+      </div>
+    </div>
+    )
+}
+
+export default ImageCard;
